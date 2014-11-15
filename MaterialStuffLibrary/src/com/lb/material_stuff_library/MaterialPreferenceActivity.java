c82c44a33e542625481646d0da4c4ba466abdc66
AndroidDeveloperLB <liran@android.co.il>
2014-11-15 14:49:59 +0200

    using elevation instead of shadow for the PreferenceActivity, in case it's Lollipop and above.

--- MaterialStuffLibrary/src/com/lb/material_stuff_library/MaterialPreferenceActivity.java
+++ MaterialStuffLibrary/src/com/lb/material_stuff_library/MaterialPreferenceActivity.java
@@ -1,10 +1,14 @@
 package com.lb.material_stuff_library;
 import android.app.Activity;
+import android.os.Build.VERSION;
+import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.widget.Toolbar;
 import android.util.TypedValue;
 import android.view.View;
+import android.view.ViewGroup;
 import com.alertdialogpro.R;
 
 public abstract class MaterialPreferenceActivity extends PreferenceActivity
@@ -25,6 +29,13 @@ public abstract class MaterialPreferenceActivity extends PreferenceActivity
     {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.msl__activity_preference);
+    if(VERSION.SDK_INT>=VERSION_CODES.LOLLIPOP)
+      {
+      _shadowView=findViewById(R.id.msl__shadowView);
+      final ViewGroup parent=(ViewGroup)_shadowView.getParent();
+      parent.removeView(_shadowView);
+      _shadowView=null;
+      }
     addPreferencesFromResource(getPreferencesXmlId());
     _toolbar=(Toolbar)findViewById(R.id.msl__toolbar);
     _toolbar.setClickable(true);
@@ -50,8 +61,13 @@ public abstract class MaterialPreferenceActivity extends PreferenceActivity
 
   protected void setEnabledActionBarShadow(final boolean enable)
     {
-    if(_shadowView==null)
-      _shadowView=findViewById(R.id.msl__shadowView);
-    _shadowView.setVisibility(enable ? View.VISIBLE : View.GONE);
+    if(VERSION.SDK_INT>=VERSION_CODES.LOLLIPOP)
+      ViewCompat.setElevation(_toolbar,enable ? 4 : 0);
+    else
+      {
+      if(_shadowView==null)
+        _shadowView=findViewById(R.id.msl__shadowView);
+      _shadowView.setVisibility(enable ? View.VISIBLE : View.GONE);
+      }
     }
   }
