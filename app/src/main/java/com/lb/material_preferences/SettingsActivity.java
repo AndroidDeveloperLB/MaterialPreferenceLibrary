73e8d9a2ef89a81e47dee115af6409315e1d8674
AndroidDeveloperLB <lblb636@gmail.com>
2015-05-26 22:37:45 +0300

    fixed some styles .

--- app/src/main/java/com/lb/material_preferences/SettingsActivity.java
+++ app/src/main/java/com/lb/material_preferences/SettingsActivity.java
@@ -2,15 +2,24 @@ package com.lb.material_preferences;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MenuItem.OnMenuItemClickListener;
+import android.view.SubMenu;
 
-import com.lb.material_preferences_library.custom_preferences.ListPreference;
 import com.lb.material_preferences_library.PreferenceActivity;
+import com.lb.material_preferences_library.custom_preferences.ListPreference;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SettingsActivity extends PreferenceActivity
   {
@@ -40,6 +49,59 @@ public class SettingsActivity extends PreferenceActivity
       return true;
       }
     });
+    handleActionBarMenu();
+
+    }
+
+  private void handleActionBarMenu()
+    {
+    getToolbar().inflateMenu(R.menu.activity_main);
+    final OnMenuItemClickListener onMenuItemClickListener=new OnMenuItemClickListener()
+    {
+    @Override
+    public boolean onMenuItemClick(final MenuItem item)
+      {
+      String url=null;
+      switch(item.getItemId())
+        {
+        case R.id.menuItem_all_my_apps:
+          url="https://play.google.com/store/apps/developer?id=AndroidDeveloperLB";
+          break;
+        case R.id.menuItem_all_my_repositories:
+          url="https://github.com/AndroidDeveloperLB";
+          break;
+        case R.id.menuItem_current_repository_website:
+          url="https://github.com/AndroidDeveloperLB/MaterialPreferenceLibrary";
+          break;
+        }
+      if(url==null)
+        return true;
+      final Intent intent=new Intent(Intent.ACTION_VIEW,Uri.parse(url));
+      intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY|Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+      intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+      startActivity(intent);
+      return true;
+      }
+    };
+    setOnMenuItemClickListenerRecursive(getToolbar().getMenu(),onMenuItemClickListener);
+    }
+
+  private static void setOnMenuItemClickListenerRecursive(Menu menu,OnMenuItemClickListener listener)
+    {
+    List<Menu> menus=new ArrayList<>(1);
+    menus.add(menu);
+    while(!menus.isEmpty())
+      {
+      final Menu currentMenu=menus.remove(0);
+      for(int i=0;i<currentMenu.size();++i)
+        {
+        final MenuItem item=currentMenu.getItem(i);
+        item.setOnMenuItemClickListener(listener);
+        final SubMenu subMenu=item.getSubMenu();
+        if(subMenu!=null)
+          menus.add(subMenu);
+        }
+      }
     }
 
   @Override
